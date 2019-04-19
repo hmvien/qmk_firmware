8881d1c5580a7d9364e7a634f26a84e7017ff85f
Hien Vien <hienmvien@gmail.com>
2019-04-18 23:00:43 -0700

    Ensure numlock is turned on when keyboard is on. Fixed numpad layer

--- keyboards/kbdfans/kbd67/hotswap/keymaps/hien/keymap.c
+++ keyboards/kbdfans/kbd67/hotswap/keymaps/hien/keymap.c
@@ -46,7 +46,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,        KC_Q,    KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSPC,          KC_DEL,  \
   MO(_FUNCTION), KC_A,    KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,                    KC_PGUP, \
   KC_LSFT,       KC_Z,    KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,                   KC_UP,   KC_PGDN, \
-  KC_LCTL,       KC_LGUI, KC_LALT,                KC_SPC,                          KC_RALT,   MO(_FUNCTION),           KC_LEFT, KC_DOWN, KC_RGHT),
+  KC_LCTL,       KC_LGUI, KC_LALT,                KC_SPC,                          KC_RALT, MO(_FUNCTION),           KC_LEFT, KC_DOWN, KC_RGHT),
 
   /* Keymap Fn Layer.
    * ,----------------------------------------------------------------.
@@ -83,11 +83,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * `----------------------------------------------------------------'`
    */
 [_NUMPAD] = LAYOUT(
-  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_PSLS, KC_PAST, KC_PMNS, KC_PEQL, KC_TRNS, KC_TRNS, KC_TRNS, \
-  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_7,    KC_8,    KC_9,   KC_PPLS, KC_TRNS, KC_BSPC,          KC_TRNS, \
-  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_4,    KC_5,    KC_6,   KC_PPLS,          KC_ENT,           KC_TRNS, \
-  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_1,    KC_2,    KC_3,   KC_TRNS,                   KC_TRNS, KC_TRNS, \
-  KC_TRNS, KC_TRNS, KC_TRNS,                   KC_TRNS,                            KC_0,    KC_DOT,                   KC_TRNS, KC_TRNS, KC_TRNS),
+  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_PSLS, KC_PAST, KC_PMNS, KC_EQL,   KC_TRNS, KC_TRNS, KC_TRNS, \
+  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_P7,    KC_P8,  KC_P9,   KC_PPLS, KC_TRNS,  KC_BSPC,          KC_TRNS, \
+  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_P4,    KC_P5,  KC_P6,   KC_PPLS,           KC_PENT,          KC_TRNS, \
+  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_P1,    KC_P2,  KC_P3,   KC_TRNS,                    KC_TRNS, KC_TRNS, \
+  KC_TRNS, KC_TRNS, KC_TRNS,                   KC_TRNS,                             KC_P0,  KC_PDOT,                    KC_TRNS, KC_TRNS, KC_TRNS),
 };
 
 
@@ -97,7 +97,10 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 }
 
 void matrix_init_user(void) {
-
+  if (!(host_keyboard_leds() & (1<<USB_LED_NUM_LOCK))) {
+    register_code(KC_NUMLOCK);
+    unregister_code(KC_NUMLOCK);
+  }
 }
 
 void matrix_scan_user(void) {
