264c91b98ac648da6deabe883abdf9140bb36825
Hien Vien <hienmvien@gmail.com>
2019-04-18 21:43:31 -0700

    Moved RESET to somewhere less convenient to prevent accidental press

--- keyboards/kbdfans/kbd67/hotswap/keymaps/hien/keymap.c
+++ keyboards/kbdfans/kbd67/hotswap/keymaps/hien/keymap.c
@@ -50,9 +50,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   /* Keymap Fn Layer.
    * ,----------------------------------------------------------------.
-   * |Rst |F1| F2| F3| F4| F5| F6| F7| F8| F9|F10|F11|F12|   |   |Prnt|
+   * |    |F1| F2| F3| F4| F5| F6| F7| F8| F9|F10|F11|F12|   |   |Prnt|
    * |----------------------------------------------------------------|
-   * |     |   |   |   |   |   |   |   |   |   |   |   |   |     |    |
+   * |     |   |   |   |   |   |   |   |   |   |   |   |   |     |Rst |
    * |----------------------------------------------------------------|
    * |       |   |   |   |   |   |   |   |   |   |   |   |       |    |
    * |----------------------------------------------------------------|
@@ -62,8 +62,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * `----------------------------------------------------------------'
    */
 [_FUNCTION] = LAYOUT(
-  RESET,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_TRNS, KC_TRNS,  KC_PSCR, \
-  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,           KC_TRNS, \
+  KC_TRNS, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_TRNS, KC_TRNS,  KC_PSCR, \
+  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,           RESET, \
   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,          KC_TRNS,           KC_TRNS, \
   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                    KC_TRNS, KC_TRNS, \
   KC_TRNS, KC_TRNS, KC_TRNS,                   KC_TRNS,                            KC_TRNS, KC_TRNS,                   KC_TRNS,  KC_TRNS, KC_TRNS),
